fd9bc85d33d4aeea68000b2478b0022bb2ebaf86
aryakdubey-11 <aryak.dubey@novostack.com>
2026-06-28 14:36:48 +0530

    updated script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/app
 
-aws ecr get-login-password --region ap-south-1 | \
-docker login --username AWS --password-stdin <ECR_URI>
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 489922707121.dkr.ecr.ap-south-1.amazonaws.com
 
-docker pull <ECR_URI>:latest
+docker pull 489922707121.dkr.ecr.ap-south-1.amazonaws.com/node-bluegreen-app:latest
 
-docker run -d -p 3000:3000 --name app <ECR_URI>:latest
+docker run -d -p 3000:3000 --name app 489922707121.dkr.ecr.ap-south-1.amazonaws.com/node-bluegreen-app:latest
