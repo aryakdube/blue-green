79b933fe0bea3d483c9eee3e352a2b1ce25808fc
Aryak Dubey <131558023+aryakdube@users.noreply.github.com>
2026-06-29 14:43:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from BLUE-GREEN Deployment 🚀");
+    res.send("Hello from Aryak 🚀");
 });
 
 app.listen(3000, () => {
